44efe58c9e48e04a6139f17fb035b55efdee4994
KVMouli <venkatmouli912310@gmail.com>
2024-10-19 17:23:19 +0530

    First Step

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
@@ -26,4 +27,22 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, StringCalculator-TDD');
   });
+  it('should return 0 for an empty string', () => {
+    expect(component.Add('')).toBe(0);
+  });
+
+  it('should return the number itself when one number is provided', () => {
+    expect(component.Add('1')).toBe(1);
+    expect(component.Add('5')).toBe(5);
+  });
+
+  it('should return the sum of two numbers', () => {
+    expect(component.Add('1,2')).toBe(3);
+    expect(component.Add('10,20')).toBe(30);
+  });
+
+  it('should handle multiple numbers (refactor when you reach this)', () => {
+    expect(component.Add('1,2,3')).toBe(6);
+  });
+
 });
